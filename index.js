4f5beacafd1154b0b5116349545050654c47234b
raymond <raymond.korir@student.moringaschool.com>
2024-04-05 10:16:03 +0300

    javascript event-lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    const leftPosition = parseInt(dodger.style.left, 10);
+
+
+if (leftPosition > 0) {
+    dodger.style.left = `${leftPosition - 1}px`;
+}
+}
+
+function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    const leftPosition = parseInt(dodger.style.left, 10);
+
+if (leftPosition < 360) {
+    dodger.style.left = `${leftPosition + 1}px`;
+}
+}
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
